22cf84f75faf4c8b931e4324cfb55ebfe9315b79
Rodrigo Brunet <rodrigosistemas2016@gmail.com>
2023-04-09 10:30:18 -0300

    font thema

---
+++ lib/const/icons/icons_package_change_theme.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/widgets.dart';
+
+class IconsTheme {
+  IconsTheme._();
+
+  static const _kFontFam = 'IconsTheme';
+  static const String? _kFontPkg = null;
+
+  static const IconData wb_sunny =
+      IconData(0xe800, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData moon =
+      IconData(0xf186, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+}
